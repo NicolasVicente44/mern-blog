fb605948f3ac88004b1c7c25502f38719873d057
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-09-28 10:37:49 -0400

    header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,43 @@
 import React, { useState } from "react";
 import { images } from "../constants";
 import { AiOutlineClose, AiOutlineMenuUnfold, IoClose } from "react-icons/ai";
+import { MdKeyboardArrowDown } from "react-icons";
 
 const navItemsInfo = [
-  { name: "Home" },
-  { name: "Articles" },
-  { name: "Pages" },
-  { name: "Pricing" },
-  { name: "FAQ" },
+  { name: "Home", type: "link" },
+  { name: "Articles", type: "link" },
+  { name: "Pages", type: "dropdown", items: ["About us, Contact us"] },
+  { name: "Pricing", type: "link" },
+  { name: "FAQ", type: "link" },
 ];
 
-const NavItem = ({ name }) => {
+const NavItem = ({ item }) => {
   return (
     <li className="relative group">
-      <a href="/" className="px-4 py-2">
-        {name}
-      </a>
-      <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
-        /
-      </span>
+      {item.type === "link" ? (
+        <>
+          <a href="/" className="px-4 py-2">
+            {item.name}
+          </a>
+          <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+            /
+          </span>
+        </>
+      ) : (
+        <>
+          <a href="/" className="px-4 py-2">
+            {item.name}
+          </a>
+          <div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max">
+            {/* Content for non-link item */}
+            <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
+              {item.items.map((page) => {
+                return <li></li>;
+              })}
+            </ul>
+          </div>
+        </>
+      )}
     </li>
   );
 };
@@ -38,9 +57,12 @@ const Header = () => {
         <div>
           <img className="w-16" src={images.Logo} alt="logo" />
         </div>
-        <div>
+        <div className="lg:hidden z-50">
           {navIsVisible ? (
-            <AiOutlineClose className="w-6 h-6" onClick={navVisibilityHandler} />
+            <AiOutlineClose
+              className="w-6 h-6"
+              onClick={navVisibilityHandler}
+            />
           ) : (
             <AiOutlineMenuUnfold
               className="w-6 h-6"
@@ -51,13 +73,16 @@ const Header = () => {
         <div
           className={`${
             navIsVisible ? "right-0" : "-right-full"
-          } flex  flex-col w-full lg:w-auto lg:flex-row justify-center lg:justify-end fixed top-0 bottom-0  lg:static  gap-x-9 items-center`}
+          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent flex z-[49]  flex-col w-full lg:w-auto lg:flex-row justify-center lg:justify-end fixed top-0 bottom-0  lg:static  gap-x-9 items-center`}
         >
-          <ul className="flex gap-x-2 font-semibold">
+          <ul className=" text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
             {navItemsInfo.map((item) => (
-              <NavItem key={item.name} name={item.name} />
+              <NavItem key={item.name} item={item} />
             ))}
           </ul>
+          <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
+            Sign in
+          </button>
         </div>
       </header>
     </section>
