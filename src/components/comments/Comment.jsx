226c56c4e66dfd16ed233a055bbb50870b3e6729
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-11-01 10:18:53 -0400

    comment jsx updated

--- src/components/comments/Comment.jsx
+++ src/components/comments/Comment.jsx
@@ -37,7 +37,7 @@ const Comment = ({
     <div className="flex flex-nowrap items-start gap-x-3 bg-[#F2F4F5] p-3 rounded-lg">
       <img
         src={images.postprofile1}
-        alt="usersprofileiamge"
+        alt="usersprofileimage"
         className="w-9 h-9 object-cover rounded-full"
       />
       <div className="flex-1 flex flex-col">
