9c489d9b9d38ecac8913e903fee07213aee10e23
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-11-14 14:43:35 -0500

    sign up view with client side validation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import HomePage from "./pages/home/HomePage";
 import { Routes, Route } from "react-router-dom";
 import ArticleDetailPage from "./pages/home/articleDetail/ArticleDetailPage";
+import ResgisterPage from "./pages/home/register/ResgisterPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index path="/" element={<HomePage />} />
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
+        <Route path="/register" element={<ResgisterPage />} />
       </Routes>
     </div>
   );
--- src/pages/home/container/Article.jsx
+++ src/pages/home/container/Article.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ArticleCard from "../../../components/ArticleCard";
 import { FaArrowRight } from "react-icons/fa";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { useDispatch } from "react-redux";
+import { changeCount } from "../../../store/actions/countActions";
 
 const Article = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+
+  const countChangeHandler = (type) => {
+    dispatch(changeCount(type));
+  };
   return (
     <section className="flex flex-col container mx-auto px-5 py-10">
       <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
-        <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
@@ -20,6 +23,11 @@ const Article = () => {
         <span>More Articles</span>
         <FaArrowRight className="w-3 h-3" />
       </button>
+      <div className="mt-2 flex items-center gap-x-5">
+        <button onClick={() => countChangeHandler("DECREASE")}>decrease</button>
+        {count.number}
+        <button onClick={() => countChangeHandler("INCREASE")}>increase</button>
+      </div>
     </section>
   );
 };
